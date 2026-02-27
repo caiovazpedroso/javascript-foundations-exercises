34a9fc4d0b31e09e1583a8d19b7c086a82f09d8e
Caio Pedroso <caiovazpedroso@outlook.com>
2026-02-26 23:30:52 -0300

    completed 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...item) {
+    let newArr = [];
+    arr.forEach((i) => {
+    if (!item.includes(i)) {
+      newArr.push(i);
+    }
+  });
+  // and return that array
+  return newArr;
 };
 
 // Do not edit below this line
