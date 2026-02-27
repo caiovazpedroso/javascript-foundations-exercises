666f88d8cd9cfaf549f0a704b6dfcc344200d6d4
Caio Pedroso <caiovazpedroso@outlook.com>
2026-02-26 23:46:17 -0300

    passes 8/14

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(a, b) {
-    let total = 0
-    for (i = a; i <= b; i++) {
+    let total = min = max = 0
+    if (a > b) {
+        max = a; min = b
+    } else {
+        min = b; min = a
+    }
+    for (i = min; i <= max; i++) {
         total += i
     }
+    return total
 };
 
 // Do not edit below this line
