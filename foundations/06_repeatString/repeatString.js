819abd58a51e43a7e10ce0312260cf758c461873
Caio Pedroso <caiovazpedroso@outlook.com>
2026-02-26 23:00:09 -0300

    completed 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return 'ERROR'
+    }
+    let x = ''
+    for (i = 0; i < n; i++) {
+        x += str
+    }
+    return x
 };
 
 // Do not edit below this line
